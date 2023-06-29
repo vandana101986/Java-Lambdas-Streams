25a57e28d5241ac153047f16dd0272b2204bced3
wanda <vandana.p10@gmail.com>
2023-06-29 11:41:18 +0530

    Final codes

--- src/com/gl/lambdaStream/Main.java
+++ src/com/gl/lambdaStream/Main.java
@@ -4,13 +4,29 @@ import java.util.List;
 
 public class Main {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		
 		PlanetUtils utils = new PlanetUtils();
+		
 		List<Planet> beginningWithM = utils.getPlanetsBeginnningWithM(SolarSystem.getPlanets());
+		System.out.println("Has name beginning with 'M' : ");
+		for (Planet p : beginningWithM) {		
+			System.out.println(p.getName());
+		}
 		List<Planet> hasMoreThanThreeMoons = utils.getPlanetsWithMoreThanThreeMoons(SolarSystem.getPlanets());
-		//System.out.println("Has Rings : "+utils.getPlanetsWithRings(SolarSystem.getPlanets()));
-		System.out.println("Has Rings : "+ hasMoreThanThreeMoons);
+		System.out.println("Has more than 3 Moons : ");
+		for (Planet p : hasMoreThanThreeMoons) {		
+			System.out.println(p.getName());
+		}
+		
+		List<Planet> hasRings = utils.getPlanetsWithRings(SolarSystem.getPlanets());
+		System.out.println("Has rings : ");
+		for (Planet p : hasRings) {		
+			System.out.println(p.getName());
+		}
 	}
 
 }
